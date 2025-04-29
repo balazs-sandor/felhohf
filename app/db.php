a0ee268024a3ca13d8da8db332cb68ef04beb808
baluson <baluson@sch.bme.hu>
2025-04-29 16:36:47 +0200

    nodebug db

--- app/db.php
+++ app/db.php
@@ -4,11 +4,6 @@ $user = getenv('DB_USER') ?: 'root';
 $pass = getenv('DB_PASSWORD') ?: '';
 $db   = getenv('DB_NAME') ?: 'image_service';
 
-echo "Host: $host\n";
-echo "User: $user\n";
-echo "Password: $pass\n";
-echo "Database: $db\n";
-
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
